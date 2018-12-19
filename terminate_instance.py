ada4fbd6f1f48ddb79654ed6903d4a2b3b4566de
lenetsarthur <46006381+lenetsarthur@users.noreply.github.com>
2018-12-19 21:15:10 +0200

    Update terminate_instance.py

--- terminate_instance.py
+++ terminate_instance.py
@@ -2,8 +2,8 @@
 import sys
 import boto3
 
-ec2 = boto3.resource('ec2', aws_access_key_id='AKIAJMFYUSHZ7VJR2WWA',
-    aws_secret_access_key='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',
+ec2 = boto3.resource('ec2', aws_access_key_id='',
+    aws_secret_access_key='',
     region_name='us-west-2')
 for instance_id in sys.argv[1:]:
     instance = ec2.Instance(instance_id)
